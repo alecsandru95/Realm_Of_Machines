1f4d0a3acfc74162810dd062d5d2bfa58a90e1a9
Ionut-Alexandru Tudor <ionut95alex@gmail.com>
2020-06-03 18:44:07 +0300

    Basic Block and Entity

---
+++ Assets/Scripts/Logging/Log.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+using UnityEngine;
+
+public static class Log
+{
+	public static readonly string[] _MessageTyes = { "INFO", "WARNING", "ERROR" };
+
+	public static void WriteLine(string line)
+	{
+		WriteLineInternal(line);
+	}
+	private static void WriteLineInternal(string line)
+	{
+		Debug.Log($"[{Thread.CurrentThread.ManagedThreadId,2}] [{_MessageTyes[0],-7}] {line}");
+	}
+}
